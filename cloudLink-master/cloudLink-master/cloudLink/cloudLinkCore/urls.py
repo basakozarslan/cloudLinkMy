ca5ff163ea84274c933ecbdd5aab64c396dfb957
basakozarslan <149895159+basakozarslan@users.noreply.github.com>
2024-01-08 16:59:07 +0200

    Update urls.py

--- cloudLink-master/cloudLink-master/cloudLink/cloudLinkCore/urls.py
+++ cloudLink-master/cloudLink-master/cloudLink/cloudLinkCore/urls.py
@@ -4,6 +4,6 @@ from .views import shorten_url, redirect_to_long_url, delete_old_records
 
 urlpatterns = [
     path('', shorten_url, name='shorten_url'),
-    path('<str:short_code>/', redirect_to_long_url, name='redirect_to_long_url'),
     path('delete_old_records/', delete_old_records, name='delete_old_records'),
+    path('<str:short_code>/', redirect_to_long_url, name='redirect_to_long_url'),
 ]
